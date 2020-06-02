2fd7b8a67f2d4d4088e5c6c345e3890ea40e14e7
Liza Star <lizadstar18@gmail.com>
2020-06-02 14:39:49 -0500

    some styling changes

--- src/javascripts/components/projectCards/projectCards.js
+++ src/javascripts/components/projectCards/projectCards.js
@@ -7,16 +7,23 @@ const createProjectCards = () => {
   projectData.getProjects().then((projects) => {
     console.error('projects', projects);
     let domString = '';
+    domString += '<div class="project-title col-12 text-center">';
+    domString += '<h1> PROJECTS </h1>';
+    domString += '</div>';
     projects.forEach((project) => {
       if (project.available !== false) {
-        domString += '<div class="card col-3">';
+        domString += '<div class="card col-3 m-2">';
+        domString += `<h2 class="card-title text-center">${project.title}</h2>`;
         domString += `<img class="card-img-top" src="${project.screenshot}" alt="Card image cap">`;
         domString += '<div class="card-body">';
-        domString += `<h2 class="card-title">${project.title}</h2>`;
+        domString += '<div class="overlay">';
+        domString += '<div class="text">';
         domString += `<p class="card-desprection">${project.description}</p>`;
         domString += `<p class="technologies"> ${project.technologiesUsed}</p>`;
         domString += `<a href="${project.url}" class="btn btn-primary"><i class="fas fa-globe"></i></i></a>`;
         domString += `<a href="${project.githubUrl}" class="btn btn-primary"><i class="fab fa-github-square"></i></a>`;
+        domString += '</div>';
+        domString += '</div>';
         domString += '</div>';
         domString += '</div>';
       }
